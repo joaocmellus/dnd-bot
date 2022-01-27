253b918f10b2dc7714484ea85c1c232e5c61c056
joaocmellus <95262089+joaocmellus@users.noreply.github.com>
2022-01-27 01:00:44 -0300

    novos dados

--- cogs/Infos.py
+++ cogs/Infos.py
@@ -1,11 +1,22 @@
-from . import Search
+from . import Search, read
 from discord import Embed
 from discord.ext import commands
+
+class InfoData(Search):
+	def __init__(self, data, data1):
+		super().__init__(data)
+		self.data1 = {i['nome'].lower() : i for i in read(data1)}
+
+	async def get_data(self, name):
+		name = name.lower()
+		if name in self.data:
+			return self.data.get(name)
+		return self.data1.get(name, None)
 
 class Infos(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.conditions = Search('conditions.json')
+		self.informations = InfoData('conditions.json', 'weapons-properties.json')
 
 	@commands.command(aliases = ['i'])
 	async def info(self, ctx, *, info = None):
@@ -13,12 +24,16 @@ class Infos(commands.Cog):
 			# Mensagem de erro
 			await ctx.send('Comando incorreto...')
 			return
-		info = await self.conditions.get_data(info)
+		info = await self.informations.get_data(info)
 
 		if not info:
 			# Mensagem de erro
 			await ctx.send('A condição não existe...')
 			return
 
-		embed = Embed(title=info['nome'], description= '• ' + '\n• '.join(info['efeitos']))		
+		if 'efeitos' in info:
+			embed = Embed(title=info['nome'], description= '• ' + '\n• '.join(info['efeitos']))
+		else:
+			embed = Embed(title=info['nome'], description= info['descrição'])
+
 		await ctx.send(embed=embed)
